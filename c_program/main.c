7c13a2b276bb4bb06641e10aa9a982e5810d947b
Owen Law <owenlaw222@gmail.com>
2024-02-22 14:58:43 -0500

    Add disassembly to c program makefile

--- c_program/main.c
+++ c_program/main.c
@@ -3,7 +3,7 @@ int test_function(int a, int b)
     return a + b;
 }
 
-void __attribute__((naked, section(".entry"))) _start(void)
+void __attribute__((naked)) _start(void)
 {
     int* ptr = (int*)0x1000;
     *ptr = test_function(-6, 5);
